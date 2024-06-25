e19c6eb02002cdee8b472d16bfea1fbdda4f4e0f
Julián Furland Otero <julian.furland@gmail.com>
2024-06-24 22:57:52 -0300

    Llenado base de datos

--- Models/BD.cs
+++ Models/BD.cs
@@ -4,7 +4,7 @@ using System.Data;
 
 public static class BD
 {
-    public static string _connectionString = @"Server=DESKTOP-KPRKEE1\SQLEXPRESS;
+    public static string _connectionString = @"Server=LocalHost;
     DataBase=BDGrabAByte;Trusted_Connection=True";
 
     public static List<Receta> ListaRecetas {get;set;}
